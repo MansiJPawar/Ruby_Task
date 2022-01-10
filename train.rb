653478dda3e8c1c7c1b961b9a1b10ab045002de7
MansiJPawar <mansipawar517@gmail.com>
2022-01-10 11:45:29 +0530

    train program menu added

---
+++ train.rb
@@ -0,0 +1,43 @@
+class Train 
+$option 
+
+    def initialize(option)
+      $option = option
+    end
+
+    def print
+      case $option
+       when 1
+        puts "1 >> Book Ticket"
+        puts "Enter name"
+        passengername = gets.chomp().to_s
+        puts "Enter age"
+        passengerage = gets.chomp().to_s
+        puts "Enter depart from"
+        depart = gets.chomp().to_s
+        puts "Enter destination"
+        destination = gets.chomp().to_s
+       when 2
+        puts "2 >> View Ticket"
+       when 3
+        puts "3 >> Cancle Ticket"
+       else 
+        puts "Invalid try again"
+       end
+    end
+
+    
+    def bookticket()
+        case $option
+        when 1
+        
+        end
+    end
+
+end 
+
+puts "Enter Choice"
+option = gets.chomp().to_i
+c1 = Train.new(option)
+c1.print
+
