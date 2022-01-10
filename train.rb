891de9a3d88e7ab6c3d3afd3668836137609ca46
MansiJPawar <mansipawar517@gmail.com>
2022-01-10 19:26:43 +0530

    modified existing code

--- train.rb
+++ train.rb
@@ -9,85 +9,99 @@ $destination
 $depart
 
     #signup
-    def details(passengername,password,passwordagain)
+    def details(passengername,password,passwordagain,option)
         $name = passengername
         $password = password
         $passwordagain = passwordagain
+        $option = option
     end    
     
     #operations
     def diplay
-        case $option
-        when 1
-        puts "1 .. Book Ticket"
-        when 2
-        puts "2 .. View Ticket"
-        when 3
-        puts "3 .. Cancle Ticket"
-        else
-        puts "invalid option"
+        if $password == $passwordagain
+           puts
+           puts "1 .. Book Ticket"
+           puts "2 .. View Ticket"
+           puts "3 .. Cancle Ticket"
+
+           while 
+           case $option 
+           when 1
+           puts ".. Book Ticket.."
+           puts $name 
+           puts $passengerage
+           when 2
+           puts "2 .. View Ticket"
+           when 3
+           puts "3 .. Cancle Ticket"
+           else
+           puts "invalid option"
+           end
+           end
+        else 
+           puts "Incorrect Password"
         end
     end
 
-    def printoption
+    # def printoption
       
-      case $option
-       when 1
-        puts "1 >> Book Ticket"
-        puts $name
+    #   case $option
+    #    when 1
+    #     puts "1 >> Book Ticket"
+    #     puts $name
 
-        puts "Enter age"
-        $passengerage = gets.chomp().to_s
+    #     puts "Enter age"
+    #     $passengerage = gets.chomp().to_s
 
-        puts "Enter depart from"
-        $depart = gets.chomp().to_s
+    #     puts "Enter depart from"
+    #     $depart = gets.chomp().to_s
 
-        puts "Enter destination"
-        $destination = gets.chomp().to_s
+    #     puts "Enter destination"
+    #     $destination = gets.chomp().to_s
 
-       when 2
-        puts "2 >> View Ticket"
-        puts "Name #{name}"
-        puts "Age #{passengerage}"
-        puts "Depart #{depart}"
-        puts "Destination #{destination}"
-        puts "Date"
-        puts "Booked Succefully"
-        puts
+    #    when 2
+    #     puts "2 >> View Ticket"
+    #     puts "Name #{name}"
+    #     puts "Age #{passengerage}"
+    #     puts "Depart #{depart}"
+    #     puts "Destination #{destination}"
+    #     puts "Date"
+    #     puts "Booked Succefully"
+    #     puts
 
-       when 3
-        puts "3 >> Cancle Ticket"
+    #    when 3
+    #     puts "3 >> Cancle Ticket"
 
-       else 
-        puts "Invalid try again"
-       end
-    end
+    #    else 
+    #     puts "Invalid try again"
+    #    end
+    # end
 
     #validate password
-    def printup
+    # def printup
     
-     puts " ********* User-data ********* "
-     puts 
+    #  puts " ********* User-data ********* "
+    #  puts 
 
-     puts $name
+    #  puts $name
       
-     if $password == $passwordagain
-        puts "Done"
-     else
-        puts "Password don't match"
-     end
-    end
+    #  if $password == $passwordagain
+    #     puts "Done"
+    #  else
+    #     puts "Password don't match"
+    #  end
+    # end
 
-    #viewticket
-    def viewticket
-        puts "Name #{$name}"
-        puts "Age #{$passengerage}"
-        puts "Depart #{$depart}"
-        puts "Destination #{$destination}"
-        puts "Date"
-        puts "Booked Succefully"
-        puts
-    end
+    # #viewticket
+    # def viewticket
+    #     puts "Name #{$name}"
+    #     puts "Age #{$passengerage}"
+    #     puts "Depart #{$depart}"
+    #     puts "Destination #{$destination}"
+    #     puts "Date"
+    #     puts "Booked Succefully"
+    #     puts
+    # end
 
 end 
 
@@ -102,19 +116,9 @@ password = STDIN.noecho(&:gets).chomp
 puts "Enter Password"
 passwordagain = STDIN.noecho(&:gets).chomp
 
-# c1 = Train.new(passengername,password,passwordagain)
-# c1.printup
-
-
 #for options
 puts 
-# option.printoption
 puts "Enter Choice"
 $option = gets.chomp().to_i
-# c1 = Train.new(option)
-# c1.choose
-
-railway = Train.new
-railway.viewticket
-railway.printup
-railway.option
+c1 = Train.new
+c1.diplay
